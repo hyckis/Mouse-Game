89af7284854d3cf574060b92755e0ff1559e2ce7
hyckis <hirakobanagi@gmail.com>
2022-12-29 16:49:37 +0800

    Add files via upload

--- Blood.java
+++ Blood.java
@@ -6,21 +6,17 @@ import java.awt.*;
 public class Blood extends JPanel {
 
 	int blood = 100;
-	JLabel bloodLabel = new JLabel();
 
 	public Blood() {}
 
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		if (blood < 30)
+		if (blood < 25)
 			g.setColor(Color.red);
 		else
 			g.setColor(Color.blue);
 		g.fillRect(0, 4, blood * 7, 3);
-		bloodLabel.setText("目前血量: " + blood);
-		add(bloodLabel);
-		controllWallWithBlood();
 	}
 
 	// 碰到牆
@@ -40,9 +36,9 @@ public class Blood extends JPanel {
 	}
 
 	// 碰到愛心
-	public void getHeart(int heartType) {
+	public void getHeart() {
 		if (blood > 0) {
-			blood += heartType;
+			blood += 10;
 			if (blood > 100)
 				blood = 100;
 		}
@@ -56,29 +52,15 @@ public class Blood extends JPanel {
 			blood = 100;
 			repaint();
 			ReadMap.timer.stop();
-			Main.changeToHeartThread.interrupt();
-			Main.changeToWallThread.interrupt();
 			try {
-				new Thread(()->{Main.readMap.heartChange();}).start();
+				new Thread(()->{Main.readMap.HeartChange();}).start();
 				Thread.sleep(1000);
-				new Thread(()->{Main.readMap.wallChange();}).start();
+				new Thread(()->{Main.readMap.WallChange();}).start();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		} else
 			System.exit(0);
-	}
-	
-	// 血量低於30
-	public void controllWallWithBlood() {
-		if (blood < 30)
-			new Thread(()->{Main.readMap.grayWall();}).start();		
-		if (blood > 30)
-			new Thread(()->{Main.readMap.generalWall();}).start();			
-	}
-	
-	public int getBlood() {
-		return blood;
 	}
 
 }
--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-// 105403506資管3A何宜親。加分題: 遊戲說明、血條在30以下時變成紅色
+// 105403506資管3A何宜親。加分題: 遊戲說明、血條在25以下時變成紅色
 
 import java.awt.*;
 import javax.swing.*;
@@ -6,11 +6,6 @@ import javax.swing.*;
 public class Main {
 	
 	static ReadMap readMap = new ReadMap();
-	static Thread changeToWallThread = new Thread(()->{readMap.heartChange();});
-	static Thread changeToHeartThread = new Thread(()->{readMap.wallChange();});
-	static Object lock = new Object();
-	
-	public Main() {}
 
 	public static void main(String[] args) {
 
@@ -19,28 +14,24 @@ public class Main {
 				+ "\nNotice: hearts will change into walls in random seconds."
 				+ "\nNotice: Don't stay at the same block, or lose LP for every second."
 				+ "\nWhen reaching the end or LP is 0, the game is over.\nGood luck!";
-		
-		
-		JOptionPane.showMessageDialog(null, intro, "Welcome", JOptionPane.PLAIN_MESSAGE);
-		
-		
 		JFrame frame = new JFrame();		
 		BorderLayout mazeInterface = new BorderLayout();
+
 		frame.setLayout(mazeInterface);
-		
+		frame.add(ReadMap.blood, BorderLayout.NORTH);	// 加血條
+		frame.add(readMap, BorderLayout.CENTER);	// 加迷宮
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(700, 750);
 		frame.setResizable(false);	// 固定視窗大小
 		frame.setVisible(true);	
+
+		JOptionPane.showMessageDialog(null, intro, "Welcome", JOptionPane.PLAIN_MESSAGE);
 		
 		try {
-			frame.add(ReadMap.blood, BorderLayout.NORTH);	// 加血條
-			frame.add(readMap, BorderLayout.CENTER);	// 加迷宮
-			Thread.sleep(10);
-			readMap.run();
-			changeToWallThread.start();
+			new Thread(()->{readMap.HeartChange();}).start();
 			Thread.sleep(1000);
-			changeToHeartThread.start();
+			new Thread(()->{readMap.WallChange();}).start();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
